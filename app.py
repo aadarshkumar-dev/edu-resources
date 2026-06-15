ae3c01bdcc99ebc3978853239ee57b336c32714e
aadarshkumar-dev <dev.aadarshkumar@gmail.com>
2026-06-15 18:47:10 +0530

    fjd

--- app.py
+++ app.py
@@ -4,76 +4,9 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = "jgfjiosfjksdfjosd"
 
-
-connection = sqlite3.connect("database.db")
-cursor = connection.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS files(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    file_path TEXT,
-    file_name TEXT,
-    file_img TEXT
-)
-""")
-
-
-# cursor.execute("SELECT COUNT(*) FROM files WHERE file_name = ?", ("reflection and refraction of light",))
-# exists = cursor.fetchone()[0]
-
-# if exists == 0:
-#     cursor.execute(
-#         "INSERT INTO files(file_path, file_name, file_img) VALUES(?, ?, ?)",
-#         ("static/PDFs/Light Reflection& Refraction short notes .pdf", "reflection and refraction of light", "https://cdn.slidesharecdn.com/ss_thumbnails/reflectionandrefractionoflight-241203110753-f7d15980-thumbnail.jpg?width=640&height=640&fit=bounds")
-#     )
-connection.commit()
-
-connection.close()
-
 @app.route('/', methods=["GET", "POST"])
 def home():
-    if request.method == "POST":
-        rows = []   # only used for search results
-        query = request.form.get("query")
-        connection = sqlite3.connect("database.db")
-        connection.row_factory = sqlite3.Row
-        cursor = connection.cursor()
-        cursor.execute("""
-            SELECT * FROM files 
-            WHERE file_name LIKE ? OR file_path LIKE ?
-        """, ('%' + query + '%', '%' + query + '%'))
-        rows = cursor.fetchall()
-        connection.close()
-
-        if not rows:
-            flash(f"No files found for '{query}' ❌")
-
-        return render_template("index.html", files=rows)
-
-    # Default GET: show all files
-    connection = sqlite3.connect("database.db")
-    connection.row_factory = sqlite3.Row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM files")
-    rows = cursor.fetchall()
-    connection.close()
-    return render_template('index.html', files=rows)
-
-def get_db_connection():
-    connection = sqlite3.connect("database.db")
-    connection.row_factory = sqlite3.Row
-    return connection
-
-
-@app.route("/show_table")
-def show_files():
-    connection = sqlite3.connect("database.db")
-    connection.row_factory = sqlite3.Row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM files")
-    rows = cursor.fetchall()
-    connection.close()
-    return render_template("show_table.html", files=rows)
+    return render_template('/index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- static/css_and_js/script.js
+++ static/css_and_js/script.js
@@ -1,37 +1,9 @@
-let toggle = document.getElementsByClassName('toggle-dark-mode-w')[0];
-let toggle_img = document.getElementsByClassName('toggle-dark-mode')[0];
-let down = document.getElementsByClassName('drop-down-w')[0];
+const toggleBtn = document.getElementById('toggleBtn');
+const sidebar = document.getElementById('sidebar');
 
-let toggled = true;
-toggle.addEventListener('click', () => {
-    if (toggled) {
-        document.getElementsByTagName('body')[0].style.backgroundColor = "black";
-        document.getElementsByTagName('body')[0].style.color = "white";
-        toggled = false;
-        toggle_img.src = 'static/images/day-and-night (1).png'
-        toggle.style.backgroundColor = 'black'
-    } else {
-        document.getElementsByTagName('body')[0].style.backgroundColor = "white";
-        document.getElementsByTagName('body')[0].style.color = "black";
-        toggled = true;
-        toggle_img.src = 'static/images/day-and-night.png'
-        toggle.style.backgroundColor = 'white'
-    }
-})
-
-
-let menu = document.getElementsByClassName('menu')[0];
-
-menu.style.visibility = "hidden";
-down.addEventListener('click', () => {
-    if (menu.style.visibility == "hidden") {
-        menu.style.visibility = "inherit";
-        down.classList.add('dropAnime')
-    } else {
-        menu.style.visibility = "hidden";
-        down.classList.remove('dropAnime')
-    }
-})
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
 
 
 let notify = document.getElementById("notify");
